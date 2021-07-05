1376e6b403182c5172855d9713a2e08389568c52
Nik-web12 <hingwe90@gmail.com>
2021-07-05 10:50:35 +0530

    first commit

---
+++ app/js/app.js
@@ -0,0 +1,51 @@
+
+gsap.registerPlugin(ScrollTrigger);
+// *****************  Progress bar ********************* \\
+gsap.to('progress', {
+    value: 100,
+    ease: 'none', 
+    scrollTrigger: { scrub: 0.3}
+});
+
+
+// ***************** Loading Animation ****************** \\
+let intro = document.querySelector('.top');
+let rotate = document.querySelector('.top-l')
+window.addEventListener('load',()=>{
+    document.body.classList.add('overflow')
+    gsap.from('.top-t',{
+        opacity:0,
+        x:-600,
+        duration:3,
+    })
+    gsap.from('.line',{
+        height:0,
+        duration:2,
+    })
+    setTimeout(()=>{
+        rotate.classList.add('rotate')
+    },100)
+    setTimeout(()=>{
+        intro.style.transform = 'translateY(-100%)'
+        // intro.style.opacity = '0.5'
+        document.body.classList.remove('overflow')
+    },3700)
+})
+
+
+// ************** Dark Mode Toggle ***************** \\
+const toggle = document.querySelector('.toggle');
+const slider = document.querySelector('.ball-slider');
+const mainTag = document.querySelector('main')
+const footer = document.querySelector('footer')
+const namingAudIn = document.querySelector('#naming-audIn')
+const namingAudOut = document.querySelector('#naming-audOut')
+
+toggle.addEventListener('click',()=>{
+    slider.classList.toggle('on')
+    mainTag.classList.toggle('main-tag')
+    footer.classList.toggle('m')
+    namingAudOut.play();
+})
+// ***************************************************** \\
+
